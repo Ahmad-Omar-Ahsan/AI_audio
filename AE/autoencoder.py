bdf86389d89083538c1d119c9dd0313a3121880b
Ahmad Omar Ahsan <gigablaze662@gmail.com>
2021-01-11 23:42:22 +0600

    Added compiling,training function

--- AE/autoencoder.py
+++ AE/autoencoder.py
@@ -2,6 +2,8 @@ from tensorflow.keras import Model
 from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization, Flatten, Dense, Reshape,Conv2DTranspose, Activation
 from tensorflow.keras import backend as K
 import numpy as np
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import MeanSquaredError
 
 
 class Autoencoder(object):
@@ -20,6 +22,7 @@ class Autoencoder(object):
         self.decoder = None
         self.model = None
         self._shape_before_bottleneck = None
+        self._model_input = None
 
         self._num_conv_layers = len(conv_filters)
 
@@ -29,12 +32,30 @@ class Autoencoder(object):
     def summary(self):
         self.encoder.summary()
         self.decoder.summary()
+        self.model.summary()
+
+
+    def compile(self, learning_rate=0.0001):
+        optimizer = Adam(learning_rate=learning_rate)
+        mse_loss = MeanSquaredError()
+        self.model.compile(optimizer=optimizer, loss=mse_loss)
+    
+
+    def train(self, x_train, batch_size, num_epochs):
+        self.model.fit(x_train, x_train, batch_size=batch_size,epochs=num_epochs,shuffle=True)
 
 
     def _build(self):
         self._build_encoder()
         self._build_decoder()
-        #self._build_autoencoder()
+        self._build_autoencoder()
+
+
+    def _build_autoencoder(self):
+        model_input =  self._model_input
+        model_output  = self.decoder(self.encoder(model_input))
+        self.model= Model(model_input, model_output, name='autoencoder')
+
 
 
     def _build_decoder(self):
@@ -102,6 +123,7 @@ class Autoencoder(object):
         encoder_input = self._add_encoder_input()
         conv_layers = self._add_conv_layers(encoder_input)
         bottleneck = self._add_bottleneck(conv_layers)
+        self._model_input = encoder_input
         self.encoder = Model(encoder_input, bottleneck, name='encoder')
 
 
